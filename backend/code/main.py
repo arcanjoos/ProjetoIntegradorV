ae35407a80cfb32641afe2973af9fb72a7e6cfeb
joao <joaoreis1@unipam.edu.br>
2021-05-11 00:09:24 -0300

    correção envio email backend

--- backend/code/main.py
+++ backend/code/main.py
@@ -23,6 +23,7 @@ from flask import Flask, jsonify, request
 from flask_cors import CORS
 from imutils import paths
 
+
 # 'INICIANDO API'
 app = Flask(__name__)
 CORS(app)
@@ -32,7 +33,7 @@ firebase_admin.initialize_app(cred)
 db = firestore.client()
 
 
-@app.route('/api')
+@app.route('/')
 def hello():
     rotas = {
         '/api/clientes': '/api/clientes',
@@ -40,8 +41,8 @@ def hello():
         '/api/pedidos': '/api/pedidos',
         '/api/mensagens': '/api/mensagens',
         '/api/mensagens/:id': '/api/mensagens/:id',
-        '/api/enviar-email': '/api/enviar-email',
-        '/api/solicitar-relatorio': '/api/solicitar-relatorio'
+        '/api/enviar-email': '/api/enviar-email/tipo',
+        '/api/solicitar-relatorio': '/api/solicitar-relatorio/tipo'
     }
 
     return jsonify(rotas)
@@ -54,7 +55,7 @@ def get_clientes():
     docs_clientes = clientes_ref.stream()
 
     clientes = []
-    endereco_entrega = []
+    # endereco_entrega = []
 
     for doc in docs_clientes:
         doc_convertido = doc.to_dict()
@@ -123,31 +124,36 @@ def get_pedidos():
 
     print(docs_pedidos)
 
-    pedidos = []
+    pedidos = [ ]
+ 
 
     for doc in docs_pedidos:
-
         # CONVERTER EM DICIONARIO
         doc_convertido = doc.to_dict()
-
+        
+        # ALTERAR O TEXTO DA TABELA 
         finalizado = ''
         if (doc_convertido['finalizado'] == True):
             finalizado = 'Sim'
         else:
             finalizado = 'Não'
 
-        # ADICIONANDO AO ARRAY
-        pedidos.append(
-            {
-                'idPedido':  doc.id,
-                'data': doc_convertido['data'],
-                'finalizado': finalizado,
-                'formaPagamento': doc_convertido['formaPagamento'],
-                'idCliente': doc_convertido['idCliente'],
-                'itens': doc_convertido['itens'],
-                'observacao': doc_convertido['observacao'],
-            }
-        )
+        for item in doc_convertido['itens']:
+
+         
+            pedidos.append({   
+                    # 'codigoPedido': contador,
+                    'idPedido':  doc.id,
+                    'data': doc_convertido['data'],
+                    'finalizado': finalizado,
+                    'formaPagamento': doc_convertido['formaPagamento'],
+                    'idCliente': doc_convertido['idCliente'],
+                    'idProduto': item['id'],
+                    'nome': item['nome'],
+                    'quantidade': item['quantidade'],
+                    'preco': item['preco'],
+                    'observacao': doc_convertido['observacao'],
+            })
 
     # return 'docs_pedidos'
     return jsonify(pedidos)
@@ -206,7 +212,6 @@ def get_mensagem_por_id(id):
 
     return jsonify(mensagens)
  
-
 def adiciona_anexo(msg, filename):
     if not os.path.isfile(filename):
         return
@@ -237,8 +242,7 @@ def adiciona_anexo(msg, filename):
     mime.add_header('Content-Disposition', 'attachment', filename=filename)
     msg.attach(mime)
 
-
-@app.route('/api/enviar-email', methods=['POST'])
+@app.route('/api/enviar-email/', methods=['POST'])
 def enviar_email():
 
     # DADOS PARA CONEXAO COM O SERVIDOR
@@ -273,15 +277,15 @@ def enviar_email():
 
     # VALIDAÇÕES BÁSICAS DOS CAMPOS
 
-    if (anexo == "" or anexo == None):
+    if (anexo == '' or anexo == None):
         anexo = False
         return anexo
 
-    if (host == "" or host == None):
+    if (host == '' or host == None):
         mensagem_erro = {'mensagem': 'Campo host é obrigatório', 'status': 404}
         return jsonify(mensagem_erro)
 
-    if (porta == "" or porta == None):
+    if (porta == '' or porta == None):
         mensagem_erro = {
             'mensagem': 'Campo porta é obrigatório', 'status': 404}
         return jsonify(mensagem_erro)
@@ -313,23 +317,7 @@ def enviar_email():
     msg.attach(MIMEText(mensagem_email, 'html', 'utf-8'))
 
     raw = msg.as_string()
-
-    if(anexo == True):
-
-        pasta_base = f'./../anexos'
-
-        lista_nome_arquivos = []
-        # ADICIONA OS ARQUIVOS DA PASTA ANEXOS COMO ANEXOS DO EMAIL.
-        for diretorio, subpastas, arquivos in os.walk(pasta_base):
-            for arquivo in arquivos:
-                print(caminho_arquivo)
-                caminho_arquivo = os.path.join(
-                os.path.dirname(diretorio), arquivo)
-
-        for i in lista_nome_arquivos:
-            print(i)
-            # adiciona_anexo(msg, i)
-
+  
     # ENVIO PROPRIAMENTE DITO DO EMAIL
     smtp = smtplib.SMTP_SSL(host, porta)
     smtp.login(usuario, senha)
@@ -339,49 +327,168 @@ def enviar_email():
     return jsonify({'mensagem': 'Email enviado com sucesso', 'status': 200})
 
 
-@app.route('/api/solicitar-relatorio')
-def enviar_relatorio():
-    # ANÁLISE DE DADOS
-    # IMPORTAÇÃO DAS BASES DE DADOS
-    tabelaProdutos = pd.read_json(r'http://localhost:8080/api/produtos')
-    tabelaClientes = pd.read_json(r'http://localhost:8080/api/clientes')
-    tabelaPedidos = pd.read_json(r'http://localhost:8080/api/pedidos')
+@app.route('/api/gerar-relatorio/<tipo>', methods=['POST'])
+def enviar_relatorio(tipo):
+    # DADOS PARA CONEXAO COM O SERVIDOR
+    usuario = ''
+    senha = ''
+    porta = ''
+    host = ''
+    enviar = ''
 
-    # TRATAMENTO DOS DADOS
-    # TRATAMENTO TABELA DE PRODUTOS
-    tabelaProdutos = tabelaProdutos.drop('idProduto', axis=1)
-    tabelaProduto = tabelaProdutos.drop('imagem', axis=1)
+    # DADOS ENVIO MENSAGEM
+    nome = ''
+    remetente = ''
+    destinatario = ''
+    assunto = ''
+    mensagem = ''
+    anexo = ''
 
-    # TRATAMENTO TABELA DE CLIENTES
-    tabelaCliente = tabelaClientes.drop('idCliente', axis=1)
+    dados = json.loads(request.data)
 
-    # TRATAMENTO TABELA DE PEDIDOS
-    tabelaPedidos = tabelaPedidos.drop('idPedido', axis=1)
-    tabelaPedido = tabelaPedidos.drop('idCliente', axis=1)
+    # DADOS PARA CONEXAO COM O SERVIDOR
+    usuario = dados['usuario']
+    senha = dados['senha']
+    porta = dados['porta']
+    host = dados['host']
 
-    # ANALISE DE DADOS
-    # CALCULANDO QUNATIDADE DE ITENS EM CADA TABELA
-    quantidadeClientesCadastrados = tabelaCliente['nome'].count()
-    quantidadeProdutosCadastrados = tabelaProduto['nome'].count()
-    quantidadePedidosRealizados = tabelaPedido.loc[tabelaPedido['finalizado'] == True].count(
-    )
-    quantidadePedidosFinalizados = tabelaPedido['finalizado'].count()
-
-    # Calculando valor total vendido
-    # totalPedidos = tabelaPedido['total'].sum()
-
-    # EXPORTAÇÃO DA PLANILHA
-    tabelaProduto.to_excel('./anexos/tabelaProdutos.xlsx', index=False)
-    tabelaCliente.to_excel('./anexos/tabelaClientes.xlsx', index=False)
-    tabelaPedido.to_excel('./anexos/tabelaPedidos.xlsx', index=False)
-
-    # adiciona_anexo(msg, './../anexos/tabelaProdutos.xlsx')
-    # adiciona_anexo(msg, './../anexos/tabelaClientes.xlsx')
-    # adiciona_anexo(msg, './../anexos/tabelaPedidos.xlsx')
+    # DADOS ENVIO MENSAGEM
+    nome = dados['nome']
+    remetente = dados['remetente']
+    destinatario = dados['destinatario']
+    assunto = dados['assunto']
+    mensagem = dados['mensagem']
+    anexo = dados['anexo']
+    enviar = dados['enviar']
 
 
+    msg = MIMEMultipart() 
+    msg['From'] = remetente
+    msg['To'] = ', '.join(destinatario)
+    msg['Subject'] = assunto
+    
+    # SELECIONANDO MENSAGEM A SER ENVIADA
+    mensagem_email = mensagem
 
-    return jsonify({'mensagem': 'Relatório salvo com sucesso', 'status': 200})
+    msg.attach(MIMEText(mensagem_email, 'html', 'utf-8'))
+    # VALIDAÇÕES BÁSICAS DOS CAMPOS
+   
+
+    if (anexo == '' or anexo == None):
+        anexo = False
+        return anexo
+
+    if (host == '' or host == None):
+        mensagem_erro = {'mensagem': 'Campo host é obrigatório', 'status': 404}
+        return jsonify(mensagem_erro)
+
+    if (porta == '' or porta == None):
+        mensagem_erro = {
+            'mensagem': 'Campo porta é obrigatório', 'status': 404}
+        return jsonify(mensagem_erro)
+
+    if ((usuario == '' and remetente == '') or (usuario == None and remetente == None)):
+        mensagem_erro = {
+            'mensagem': 'Campo usuário é obrigatório', 'status': 404}
+        return jsonify(mensagem_erro)
+
+    if (senha == '' or senha == None):
+        mensagem_erro = {
+            'mensagem': 'Campo senha é obrigatório', 'status': 404}
+        return jsonify(mensagem_erro)
+
+    if (assunto == '' or assunto == None):
+        mensagem_erro = {
+            'mensagem': 'Campo assunto é obrigatório', 'status': 404}
+        return jsonify(mensagem_erro)
+
+
+    if(tipo == 'clientes'): 
+        # IMPORTAÇÃO DAS BASES DE DADOS
+        tabelaClientes = pd.read_json(r'http://localhost:8080/api/clientes')
+
+        # TRATAMENTO TABELA DE CLIENTES
+        tabelaClientes = tabelaClientes.drop('idCliente', axis=1)
+ 
+        # ANALISE DE DADOS
+        # CALCULANDO QUNATIDADE DE ITENS EM CADA TABELA
+        quantidadeClientesCadastrados = tabelaClientes['nome'].count() 
+
+        tabelaClientes.to_excel('./anexos/tabelaClientes.xlsx', index=False)
+
+
+        if(enviar == True):
+            # ADICIONAR ARQUIVOS ANEXOS
+            adiciona_anexo(msg, 'anexos/tabelaClientes.xlsx')  
+
+    if(tipo == 'produtos'): 
+        # IMPORTAÇÃO DAS BASES DE DADOS
+        tabelaProdutos = pd.read_json(r'http://localhost:8080/api/produtos')
+ 
+        # TRATAMENTO TABELA DE PRODUTOS
+        tabelaProdutos = tabelaProdutos.drop('idProduto', axis=1)
+        tabelaProdutos = tabelaProdutos.drop('imagem', axis=1)
+
+        # ANALISE DE DADOS
+        # CALCULANDO QUNATIDADE DE ITENS EM CADA TABELA
+        quantidadeProdutosCadastrados = tabelaProdutos['nome'].count() 
+ 
+        # EXPORTAÇÃO DA PLANILHA
+        tabelaProdutos.to_excel('./anexos/tabelaProdutos.xlsx', index=False)
+
+
+        if(enviar == True):
+            # ADICIONAR ARQUIVOS ANEXOS 
+            adiciona_anexo(msg, 'tabelaProdutos.xlsx') 
+
+    if(tipo == 'pedidos'): 
+        # IMPORTAÇÃO DAS BASES DE DADOS
+        tabelaClientes = pd.read_json(r'http://localhost:8080/api/clientes')
+        tabelaProdutos = pd.read_json(r'http://localhost:8080/api/produtos')
+        tabelaPedidos = pd.read_json(r'http://localhost:8080/api/pedidos')    
+ 
+        # TRATAMENTO DOS DADOS
+        # TRATAMENTO TABELA DE PRODUTOS
+        tabelaProdutos = tabelaProdutos.drop('idProduto', axis=1)
+        tabelaProdutos = tabelaProdutos.drop('imagem', axis=1)
+
+        # TRATAMENTO TABELA DE CLIENTES
+        tabelaClientes = tabelaClientes.drop('idCliente', axis=1)
+
+        # TRATAMENTO TABELA DE PEDIDOS
+        tabelaPedidos = tabelaPedidos.drop('idPedido', axis=1)
+        tabelaPedidos = tabelaPedidos.drop('idCliente', axis=1)
+
+        # ANALISE DE DADOS
+        # CALCULANDO QUNATIDADE DE ITENS EM CADA TABELA
+        quantidadeClientesCadastrados = tabelaClientes['nome'].count()
+        quantidadeProdutosCadastrados = tabelaProdutos['nome'].count()
+        quantidadePedidosRealizados = tabelaPedidos.loc[tabelaPedidos['finalizado'] == True].count()
+    
+        tabelaClientes.to_excel('./anexos/tabelaClientes.xlsx', index=False)
+        tabelaProdutos.to_excel('./anexos/tabelaProdutos.xlsx', index=False)
+        tabelaPedidos.to_excel('./anexos/tabelaPedidos.xlsx', index=False)
+
+
+
+        if(enviar == True):
+
+            adiciona_anexo(msg, 'tabelaProdutos.xlsx')
+            # adiciona_anexo(msg, 'anexos/tabelaPedidos.xlsx')   
+
+    raw = msg.as_string()
+
+    if(enviar == True and anexo == True ):
+        # ENVIO PROPRIAMENTE DITO DO EMAIL
+        smtp = smtplib.SMTP_SSL(host, porta)
+        smtp.login(usuario, senha)
+        smtp.sendmail(remetente, destinatario, raw)
+        smtp.quit()
+    
+        return jsonify({'mensagem': 'Relatório enviado por email', 'status': 200})
+
+    else : 
+        return jsonify({'mensagem': 'Relatório salvo com sucesso', 'status': 200})
 
 
 if __name__ == '__main__':
